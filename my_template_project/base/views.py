792fdb5996b6db925afccd74c00cc3c790c902cd
Jamerzbatman <jamiethetec@gmail.com>
2024-08-13 14:50:51 -0700

    got the messaging done

--- my_template_project/base/views.py
+++ my_template_project/base/views.py
@@ -3,22 +3,28 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from django.http import JsonResponse
+
 
 
 def login_view(request):
     if request.method == 'POST':
         form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
-            # Get the user from the form
             user = form.get_user()
-            # Authenticate and log in the user
             login(request, user)
-            # Redirect to home or any other page
+            if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+                return JsonResponse({'success': True, 'redirect_url': '/dashboard/'})
             return redirect('dashboard')
+        else:
+            if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+                errors = {field: form.errors.as_json() for field in form.errors}
+                return JsonResponse({'success': False, 'errors': errors})
     else:
         form = AuthenticationForm()
 
     return render(request, 'base/login.html', {'form': form})
+
 
 
 
@@ -27,9 +33,14 @@ def register(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
+            if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+                return JsonResponse({'success': True, 'message': 'Your account has been created successfully! You can now log in.'})
             messages.success(request, 'Your account has been created successfully! You can now log in.')
             return redirect('login')
         else:
+            if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+                errors = form.errors.get_json_data()
+                return JsonResponse({'success': False, 'errors': errors})
             messages.error(request, 'Please correct the errors below.')
     else:
         form = UserCreationForm()
